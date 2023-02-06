e303de265328c9ac472ff1d17442ac73101f8a77
FelixPolkanov <106100262+FelixPolkanov@users.noreply.github.com>
2023-02-06 15:45:02 +0200

    Initial commit

---
+++ src/redux/operations.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  fetchContactsAPI,
+  addContactAPI,
+  deleteContactAPI,
+} from 'services/contactsAPI';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const contacts = await fetchContactsAPI();
+      return contacts;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (item, thunkAPI) => {
+    try {
+      const contact = await addContactAPI(item);
+      return contact;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const contact = await deleteContactAPI(contactId);
+      return contact;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
