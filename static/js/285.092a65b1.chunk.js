0694e0aaebcd462cd421644d8512ff74f2ea2918
FelixPolkanov <106100262+FelixPolkanov@users.noreply.github.com>
2023-02-12 09:07:29 +0000

    Deploying to gh-pages from @ FelixPolkanov/goit-react-hw-08-phonebook@a5eb74aff20cef7d2f544e7caa3346a3137cbda1 🚀

--- static/js/285.092a65b1.chunk.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[285],{8813:function(){},285:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return c}});var i=t(4270),r=t(8813),o=t(3329);function c(){return(0,o.jsxs)("div",{children:[(0,o.jsx)(i.q,{children:(0,o.jsx)("title",{children:"Registration"})}),(0,o.jsx)(r.RegisterForm,{})]})}}}]);
-//# sourceMappingURL=285.092a65b1.chunk.js.map
