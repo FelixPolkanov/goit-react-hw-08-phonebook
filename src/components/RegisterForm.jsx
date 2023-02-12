af121723c64504dd1878befef881f9550ebd852e
FelixPolkanov <felixpolkanov@gmail.com>
2023-02-12 19:11:57 +0200

    add task

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -66,7 +66,7 @@ export const RegisterForm = () => {
             autoComplete="name"
             autoFocus
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            title="Name ( MORE THAN 7 SYMBOLS! ) may contain only letters, apostrophe, dash and spaces."
           />
           <TextField
             margin="normal"
